c4113b0098bb70e0d41c6d2d5ade48b57dea81b0
Shriphani Palakodety <shriphanip@gmail.com>
2014-11-20 06:31:08 -0500

    expt done

--- src/questions_experiment/core.clj
+++ src/questions_experiment/core.clj
@@ -1,6 +1,34 @@
-(ns questions-experiment.core)
+(ns questions-experiment.core
+  (:require [clojure.java.io :as io])
+  (:import [com.aliasi.chunk Chunker Chunking]
+           [com.aliasi.util AbstractExternalizable]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-chunker
+  [path]
+  (AbstractExternalizable/readObject
+   (io/as-file path)))
+
+(defn load-questions
+  [path]
+  (clojure.string/split-lines
+   (slurp path)))
+
+(defn tag-questions
+  [questions-file chunker-file]
+  (let [chunker (load-chunker chunker-file)
+        questions (load-questions questions-file)
+
+        chunked
+        (map
+         (fn [q]
+           (.chunk chunker q))
+         questions)]
+
+    (map
+     (fn [c]
+       (let [cset (.chunkSet c)]
+         (map
+          (fn [s]
+            (.start s))
+          cset)))
+     chunked)))
